022d442c33e01c65c766760d092250c916b70bac
steambus-del <steambus@gmail.com>
2026-07-13 10:41:35 -0700

    Update gain-loss-history.js

--- gain-loss-history.js
+++ gain-loss-history.js
@@ -3,5 +3,5 @@ window.gainLossHistory = [
   // { date: "2026-07-15", gainLoss: 523.42 },
   { date: "2026-07-09", gainLoss: 210 },
   { date: "2026-07-10", gainLoss: 191 },
-  { date: "2026-07-13", gainLoss: 31 },
+  { date: "2026-07-13", gainLoss: -9 },
 ];
