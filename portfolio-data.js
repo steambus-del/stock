60aa39b8e63d932edeffd6964eeee9dfb03a7167
steambus-del <steambus@gmail.com>
2026-07-13 09:56:49 -0700

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -17,5 +17,7 @@ window.sharedTransactions = [
     { date: "2026-07-10", type: "buy", symbol: "NFLX", shares: 13.6587, price: 73.2132 },
     { date: "2026-07-10", type: "buy", symbol: "GOOGL", shares: 2.8219, price: 354.3712 },
     { date: "2026-07-10", type: "buy", symbol: "MU", shares: 1.0374, price: 963.9194 },
-    { date: "2026-07-10", type: "buy", symbol: "ORCL", shares: 7.0781, price: 141.2794 }
+    { date: "2026-07-10", type: "buy", symbol: "ORCL", shares: 7.0781, price: 141.2794 },
+    { date: "2026-07-10", type: "buy", symbol: "MU", shares: 1.0969, price: 911.61},
+    { date: "2026-07-10", type: "buy", symbol: "ORCL", shares: 7.3335, price: 136.36 }
 ];
