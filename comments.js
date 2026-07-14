76256177e84112247534db7c9d3ea1323dcdb179
steambus-del <steambus@gmail.com>
2026-07-14 10:36:31 -0700

    Update comments.js

--- comments.js
+++ comments.js
@@ -11,4 +11,13 @@ window.portfolioComments = [
   //   date: "2026-07-14",
   //   comment: "Consider adding DIS if it falls below the target price."
   // }
+  {
+      date: "2026-07-15",
+      comment: "MSFT（380以内加仓 370以内重仓）， MU (930以内加仓，900以内重仓），DIS(96以内加仓，93以内重仓），NVDA(200以内加仓，190以内重仓），GOOGL（350以内加仓，330以内重仓), IAU (75.5以内加仓），VZ(42以内加仓） "
+  }, 
+  
+  {
+      date: "2026-07-15",
+      comment: "暂时小心ORCL，NFLX"
+  }
 ];
