1f008fd1b6d90315493675b2bd853e861557fdfc
steambus-del <steambus@gmail.com>
2026-07-21 09:00:43 -0700

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -70,5 +70,5 @@ window.sharedTransactions = [
     { date: "2026-07-17", type: "buy", symbol: "GOOGL", shares: 5.8198, price: 343.65},
     { date: "2026-07-20", type: "buy", symbol: "DIS", shares: 20.7905, price: 96.2 },
     { date: "2026-07-21", type: "sell", symbol: "MU", shares: 33.1466, price: 929.29},
-    { date: "2026-07-17", type: "buy", symbol: "GOOGL", shares: 5.7476, price: 347.97}
+    { date: "2026-07-21", type: "buy", symbol: "GOOGL", shares: 5.7476, price: 347.97}
 ];
