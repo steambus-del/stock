3b2f536ea8cc946ef3743c8a45be216ac04c8510
steambus-del <steambus@gmail.com>
2026-07-21 13:18:25 -0700

    Update gain-loss-history.js

--- gain-loss-history.js
+++ gain-loss-history.js
@@ -10,34 +10,42 @@ window.gainLossHistory = [
   },
   {
     "date": "2026-07-20",
-    "gainLoss": -938.02
+    "gainLoss": -938.02,
+    "dailyGainLoss": 397.11
   },
   {
     "date": "2026-07-17",
-    "gainLoss": -1335.13
+    "gainLoss": -1335.13,
+    "dailyGainLoss": -610.47
   },
   {
     "date": "2026-07-16",
-    "gainLoss": -724.66
+    "gainLoss": -724.66,
+    "dailyGainLoss": -1206.21
   },
   {
     "date": "2026-07-15",
-    "gainLoss": 481.55
+    "gainLoss": 481.55,
+    "dailyGainLoss": 273.28
   },
   {
     "date": "2026-07-14",
-    "gainLoss": 208.27
+    "gainLoss": 208.27,
+    "dailyGainLoss": 303.23
   },
   {
     "date": "2026-07-13",
-    "gainLoss": -94.96
+    "gainLoss": -94.96,
+    "dailyGainLoss": -285.96
   },
   {
     "date": "2026-07-10",
-    "gainLoss": 191
+    "gainLoss": 191,
+    "dailyGainLoss": -19
   },
   {
     "date": "2026-07-09",
-    "gainLoss": 210
+    "gainLoss": 210,
+    "dailyGainLoss": 210
   }
 ];
