000c0ac1dfb02256ca307b171b2f3f58147813d2
steambus-del <steambus@gmail.com>
2026-07-23 10:50:58 -0700

    Update comments.js

--- comments.js
+++ comments.js
@@ -13,12 +13,7 @@ window.portfolioComments = [
   // }
   {
       date: "2026-07-15",
-      comment: "GOOGL（325以内加仓，310以内重仓), MSFT（375以内加仓 350以内重仓），DIS(96以内加仓，93以内重仓），NVDA(暂时不要动）， IAU (75.5以内少量加仓），VZ(42以内加仓 , 40以内重仓）, NFLX(65以内少量加仓），ORCL(120以内少量加仓） "
-  },
-
-   {
-      date: "2026-07-16",
-      comment: "大跌可大加仓：GOOGL, MSFT, MU, DIS, NVDA, VZ      少量加仓：NFLX,ORCL      酌情判断：IAU "
+      comment: "GOOGL（325以内加仓，310以内重仓), MSFT（375以内加仓 350以内重仓），DIS(92以内加仓，90以内重仓）， NFLX(67以内少量加仓），ORCL(120以内少量加仓） "
   }
   
 ];
