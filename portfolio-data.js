b1521b5abd9c2e6fce9962095c9629da6e54709f
steambus-del <steambus@gmail.com>
2026-07-29 07:34:15 -0700

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -120,5 +120,7 @@ window.sharedTransactions = [
     { date: "2026-07-27", type: "sell", symbol: "NFLX", shares: 69.7123, price: 71.03},
     { date: "2026-07-28", type: "buy", symbol: "MU", shares: 12.1967, price: 819.89},
     { date: "2026-07-28", type: "buy", symbol: "MU", shares: 2.4503, price: 816.2},
-    { date: "2026-07-28", type: "buy", symbol: "NVDA", shares: 40.6369, price: 196.86}
+    { date: "2026-07-28", type: "buy", symbol: "NVDA", shares: 40.6369, price: 196.86},
+    { date: "2026-07-28", type: "buy", symbol: "MU", shares: 12.7094, price: 786.82},
+    { date: "2026-07-28", type: "buy", symbol: "NVDA", shares: 52.237, price: 191.43}
 ];
