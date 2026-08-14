97233514c99a00b468515778143f2439fbb64352
steambus-del <steambus@gmail.com>
2026-08-14 07:40:11 -0700

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -172,5 +172,6 @@ window.sharedTransactions = [
     { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 32.6559, price: 306.22},
     { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 32.7011, price: 305.8},
     { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 16.3894, price: 305.07},
-    { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 16.4015, price: 304.85}
+    { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 16.4015, price: 304.85},
+    { date: "2026-08-14", type: "buy", symbol: "AAPL", shares: 16.4058, price: 304.77}
 ];
