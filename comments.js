db65491f7a4465d89eab6c5c4c56c6bf41fc1ff1
steambus-del <steambus@gmail.com>
2026-08-25 10:51:57 -0700

    Update comments.js

--- comments.js
+++ comments.js
@@ -12,7 +12,7 @@ window.portfolioComments = [
   //   comment: "Consider adding DIS if it falls below the target price."
   // }
   {
-      date: "2026-07-15",
+      date: "2026-08-25",
       comment: "强力买入区	$294–300	积极分批买入  买入区	$303–308	开始/增加仓位 第一止盈区	$325–330	卖出部分仓位 第二止盈区	$340–345	再卖出一部分第三止盈区	$350–360	大幅止盈  长期目标	$380–400+	保留剩余仓位"
   }
   
