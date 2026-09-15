f409b0ea2b77c9b0bc199f87fa5c1ad061b4be09
steambus-del <steambus@gmail.com>
2026-09-15 07:53:57 -0700

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -348,5 +348,6 @@ window.sharedTransactions = [
     { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 58.3192, price: 342.94},
     { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 29.1738, price: 342.77},
     { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 58.4413, price: 342.22},
-    { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 58.4986, price: 341.89}
+    { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 58.4986, price: 341.89},
+    { date: "2026-09-15", type: "buy", symbol: "AVGO", shares: 87.9262, price: 341.2}
 ];
